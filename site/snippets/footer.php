7dc5f27559a0d60e83c4e937fadc7c96c2279c30
L Daniel Swakman <d.swakman@gmail.com>
2017-10-15 19:37:09 +0200

    footer stuff

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -3,16 +3,14 @@
 
       <div class="col-xs-12 col-sm-4">
         <br><br>
-        <a href="https://www.instagram.com/balamirnazlica/" target="_blank">Instagram</a><br>
-        <a href="mailto:info@balamirnazlica.com" target="_blank">Email</a><br><br>
-        <div class="u-op30"><?= $site->copyright()->kirbytext() ?></div>
+
+        <?= $site->contact_links()->kirbytext() ?>
+
       </div>
 
       <div class="col-xs-12 col-sm-8">
 
-        <h4>Get in touch</h4>
-        <br>
-        <p>A bit of text to tell people that this is the way to get in touch w Balamir</p>
+        <?= $site->contact_form_text()->kirbytext() ?>
 
         <? snippet('contact-form') ?>
 
