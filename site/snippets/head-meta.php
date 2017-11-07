2e91addcacf0b50948550843c67053c9b72fe996
L Daniel Swakman <d.swakman@gmail.com>
2017-11-07 15:08:20 +0100

    added meta fields

--- site/snippets/head-meta.php
+++ site/snippets/head-meta.php
@@ -1,8 +1,16 @@
 <?
 $image_url = r($site->meta_image()->isNotEmpty(), $site->image($site->meta_image())->url());
+
 $title = r($page->isHomePage(), $site->title()->html(), $page->title()->html());
+if($page->meta_title()->isNotEmpty()) { $title = $page->meta_title()->html(); }
+
 $site_title = $site->title()->html();
+
 $descr = $site->description()->html();
+if($page->meta_title()->isNotEmpty()) { $descr = $page->meta_description()->html(); }
+
+$keywords = $site->keywords()->html();
+if($page->meta_keywords()->isNotEmpty()) { $keywords = $page->meta_keywords()->html(); }
 ?>
 
 <meta charset="utf-8">
@@ -12,7 +20,7 @@ $descr = $site->description()->html();
 <meta http-equiv="Cache-control" content="public">
 
 <meta name="description" content="<?= $descr ?>">
-<meta name="keywords" content="<?= $site->keywords()->html() ?>">
+<meta name="keywords" content="<?= $keywords ?>">
 
 <!-- Social share parameters -->
 <meta property="og:image" content="<?= $image_url ?>" />
